f15d207893d0b56ba09e99e9a12c78ef7d5df187
yujrajo <joanna.yujra@jala.university>
2024-11-05 17:31:36 -0500

    first app in flask with postgres db. Using Docker Compose to create both servers

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+
+    def json(self):
+        return {'id': self.id,'name': self.name, 'last_name': self.last_name}
+
+db.create_all()
+
+#create a test route
+@app.route('/test', methods=['GET'])
+def test():
+  return make_response(jsonify({'message': 'test route'}), 200)
+
+
+# create a user
+@app.route('/users', methods=['POST'])
+def create_user():
+  try:
+    data = request.get_json()
+    new_user = User(name=data['name'], last_name=data['last_name'])
+    db.session.add(new_user)
+    db.session.commit()
+    return make_response(jsonify({'message': 'user created'}), 201)
+  except e:
+    return make_response(jsonify({'message': 'error creating user'}), 500)
+
+# get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+  try:
+    users = User.query.all()
+    return make_response(jsonify([user.json() for user in users]), 200)
+  except e:
+    return make_response(jsonify({'message': 'error getting users'}), 500)
+
